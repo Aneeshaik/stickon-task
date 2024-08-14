ea0592eab166d455c5b1a91f5b9faaf061835304
Anees <aneesnafeesimmu@gmail.com>
2024-08-14 11:41:18 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [tableData, setTableData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  },[])
+  const fetchData = async() => {
+    const data = await fetch('https://randomuser.me/api/?results=100');
+    const jsonData = await data.json();
+    setTableData(jsonData.results);
+    setFilteredData(jsonData.results);
+  }
+  const changeHandler = (e) => {
+    setSearchValue(e.target.value);
+    const filteredSearch = filteredData.filter((user) => (
+      user.location.street.name.toLowerCase().includes(searchValue.toLowerCase()) || user.location.country.toLowerCase().includes(searchValue.toLowerCase())
+    ))
+    setTableData(filteredSearch);
+  }
+  // console.log(searchValue);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <center>
+      <h1>Users List</h1>
+      <input type='text' name='user' placeholder='Search...' onChange={changeHandler} value={searchValue}></input>
+      <table>
+        <thead>
+          <tr>
+            <th>city</th>
+            <th>state</th>
+            <th>country</th>
+            <th>postcode</th>
+            <th>number</th>
+            <th>name</th>
+            <th>latitude</th>
+            <th>longitude</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((user, index) => (
+            <tr key={index}>
+              <td>{user.location.city}</td>
+              <td>{user.location.state}</td>
+              <td>{user.location.country}</td>
+              <td>{user.location.postcode}</td>
+              <td>{user.location.street.number}</td>
+              <td>{user.location.street.name}</td>
+              <td>{user.location.coordinates.latitude}</td>
+              <td>{user.location.coordinates.longitude}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </center>
     </div>
   );
 }
